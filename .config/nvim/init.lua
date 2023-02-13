452d91ecb3656c3e2b5d39b6560913c15599add1
ittokun <73953416+ittokun@users.noreply.github.com>
2023-02-13 20:38:11 +0900

    Deleted Old NeoVim Settings😂

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -2,10 +2,3 @@
 vim.cmd("so ~/.vimrc")
 
 require("lazy-config")
-
--- require("lazy-config.setup")
-
--- require("lsp-config");
--- require("lsp-config.mason");
--- require("lsp-config.cmp");
--- require("lsp-config.snippets");
