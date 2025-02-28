0c6e3119c51acaf780e59a39df82bfd2c59e8de8
ittokunvim <73953416+ittokunvim@users.noreply.github.com>
2025-02-28 21:29:29 +0900

    feat: :heavy_plus_sign: Add markdown-preview.nvim

--- .config/nvim/lua/plugins/util.lua
+++ .config/nvim/lua/plugins/util.lua
@@ -6,4 +6,13 @@ return {
   },
   -- editorconfig
   "editorconfig/editorconfig-vim",
+  -- markdown preview
+  {
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    ft = { "markdown" },
+    -- 動作しなければ、Vimコマンドで`:call mkdp#util#install()`を実行する
+    build = function() vim.fn["mkdp#util#install"]() end,
+    keys = { { ";mdp", "<cmd>MarkdownPreview<cr>", desc = "Markdown Preview" } },
+  },
 }
