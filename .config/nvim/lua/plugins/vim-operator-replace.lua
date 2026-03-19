7e5e782041039f5aae41e67605c786f83d0bb985
ittokunvim <73953416+ittokunvim@users.noreply.github.com>
2026-03-19 21:30:15 +0900

    fix: Removed nvim plugins

--- .config/nvim/lua/plugins/vim-operator-replace.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "kana/vim-operator-replace",
-  dependencies = {
-    "kana/vim-operator-user",
-  },
-  keys = {
-    { "_", "<Plug>(operator-replace)", desc = "vim-operator-replace" },
-  },
-}
